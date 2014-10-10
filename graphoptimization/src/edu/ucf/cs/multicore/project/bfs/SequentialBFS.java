2a76425d602520e9c701fcc16cc96571f57b0d98
Ramin <ramizd@gmail.com>
2014-10-10 15:09:20 -0400

    remove recursive behaviour of SequentialBFS

--- graphoptimization/src/edu/ucf/cs/multicore/project/bfs/SequentialBFS.java
+++ graphoptimization/src/edu/ucf/cs/multicore/project/bfs/SequentialBFS.java
@@ -7,12 +7,13 @@ import java.util.Queue;
 import edu.ucf.cs.multicore.project.model.Node;
 import edu.ucf.cs.multicore.project.test.GraphOptimizationController;
 
-public class SequentialBFS implements BFSStrategy{
+public class SequentialBFS implements BFSStrategy {
 	private boolean[] visitedArray;
-	Queue<Node> bfsQueue=new LinkedList<Node>();
-	
+	Queue<Node> bfsQueue = new LinkedList<Node>();
+
 	public SequentialBFS() {
-		visitedArray = new boolean[GraphOptimizationController.getNumberOfNodes()];
+		visitedArray = new boolean[GraphOptimizationController
+				.getNumberOfNodes()];
 		for (int i = 0; i < visitedArray.length; i++) {
 			visitedArray[i] = false;
 		}
@@ -20,39 +21,30 @@ public class SequentialBFS implements BFSStrategy{
 
 	@Override
 	public void runBFS(Node srouce, Node dest) {
-		
-		visitedArray[srouce.getIndex()]=true;
-		if(srouce.equals(dest)){
-			
-			System.out.println("Reached the destination node");
-//			System.exit(0);
-			return;
-		}
-		else{
-			
-			List<Node> neighboringList=srouce.getNeighboringNodes();
-			
-			for(int i=0;i<neighboringList.size();i++){
-				if(!visitedArray[neighboringList.get(i).getIndex()]){
-//					System.out.println("Current node being added into the queue:"+neighboringList.get(i));
-					visitedArray[neighboringList.get(i).getIndex()]=true;
-					bfsQueue.add(neighboringList.get(i));
+
+		visitedArray[srouce.getIndex()] = true;
+		while (true) {
+			if(bfsQueue.isEmpty()){
+				bfsQueue.add(srouce);
+			}else {
+				Node queuenode = (Node) bfsQueue.remove();
+				if (queuenode.equals(dest)) {
+					System.out.println("Reached the destination node");
+					return;
 				}
-				
-			}
-			
-			try{
-				Node queuenode=(Node)bfsQueue.remove();
-				runBFS(queuenode, dest);//Recurrsive call  to the function 
-			}
-			catch(Exception e){
-				System.out.println("Error occured somewhere..Please figure out whats wrong. :D Or the nodes might not be connected");
-				e.printStackTrace();
+				List<Node> neighboringList = queuenode.getNeighboringNodes();
+
+				for (int i = 0; i < neighboringList.size(); i++) {
+					if (!visitedArray[neighboringList.get(i).getIndex()]) {
+						visitedArray[neighboringList.get(i).getIndex()] = true;
+						bfsQueue.add(neighboringList.get(i));
+					}
+				}
 			}
 		}
-				
-		
+
 	}
-	
+
+
 
 }
